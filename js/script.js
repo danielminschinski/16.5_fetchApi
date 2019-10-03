f6f513032a6373b43cc5f2569fc558567c4d8b45
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-03 22:29:33 +0200

    Update style

--- js/script.js
+++ js/script.js
@@ -10,34 +10,34 @@ function getQuote(){
 		.then(createTweet);
 }
 
-function createTweet(input){
-	var data = input[0];
+function createTweet(input) {
+    var data = input[0];
 
-	var dataElement = document.createElement('div');
-	dataElement.innerHTML = data.content;
-	var quoteText = dataElement.innerText.trim();
-	var quoteAuthor = data.title;
+    var dataElement = document.createElement('div');
+    dataElement.innerHTML = data.content;
+    var quoteText = dataElement.innerText.trim();
+    var quoteAuthor = data.title;
 
-	if(!quoteAuthor.length){
-		quoteAuthor = "Unknown author"
-	}
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
 	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    
+    if (tweetText.length > 140) {
+        getQuote();
+    } else{
 
-	if(tweetText.length > 140){
-		getQuote();
-	} else {
-		var tweet = tweetLink + encodeURIComponent(tweetText);
-		document.querySelector('.quote').innerText = quoteText;
-		document.querySelector('.author').innerText = "Author: " + quoteAuthor;
-		document.querySelector('.tweet').setAttribute('href', tweet);
-	}
-
-
+    	var tweet = tweetLink + encodeURIComponent(tweetText);
+    	document.querySelector('.quote').innerText = quoteText;
+    	document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+    	document.querySelector('.tweet').setAttribute('href', tweet);
+    }
 }
 
-document.addEventListener('DOMContentLoader', function(){
-	getQuote();
-	document.querySelector('.trigger').addEventListener('click', function(){
-		getQuote();
-	});
+document.addEventListener('DOMContentLoaded', function() {
+    getQuote();
+    document.querySelector('.trigger').addEventListener('click', function() {
+        getQuote();
+    });
 });

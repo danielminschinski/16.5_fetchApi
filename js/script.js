e69462e44cc2298560fbdac2d9f89b6f1633271d
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-06 23:17:01 +0200

    Update script

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&_=1570288279003";
 
 function getQuote(){
-	fetch(prefix + quoteUrl, { cache: "no-store" })
-		.then(function(resp){
-			return resp.json();
-		})
-		.then(createTweet);
+    fetch(prefix + quoteUrl, { cache: "no-store" })
+        .then(function(resp){
+            return resp.json();
+        })
+        .then(createTweet);
 }
 
 function createTweet(input) {
@@ -16,22 +16,22 @@ function createTweet(input) {
     var dataElement = document.createElement('div');
     dataElement.innerHTML = data.content.rendered;
     var quoteText = dataElement.innerText.trim();
-    var quoteAuthor = data.title;
+    var quoteAuthor = data.title.rendered;
 
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
 
-	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor.author;
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor.author;
     
     if (tweetText.length > 140) {
         getQuote();
     } else{
 
-    	var tweet = tweetLink + encodeURIComponent(tweetText);
-    	document.querySelector('.quote').innerText = quoteText;
-    	document.querySelector('.author').innerText = "Author: " + quoteAuthor;
-    	document.querySelector('.tweet').setAttribute('href', tweet);
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        document.querySelector('.quote').innerText = quoteText;
+        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+        document.querySelector('.tweet').setAttribute('href', tweet);
     }
 };
 
